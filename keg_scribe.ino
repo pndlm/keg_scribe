df9e90c33d57d36097d33a1820ac78adccad0bcf
Allen Peloquin <acpeloquin@gmail.com>
2014-04-18 14:02:19 -0700

    Wifi Shield Acceptance --- This should be a dead-end branch that proves we have our CC3000 working properly. Next step is to take the core of their WiFi connection behavior and strip it down, re-integrating the basic flow meter work back on master.

--- keg_scribe.ino
+++ keg_scribe.ino
@@ -1,111 +1,197 @@
-/**********************************************************
-This is example code for using the Adafruit liquid flow meters. 
+/*************************************************** 
+  Client_fastrprint test
 
-Tested and works great with the Adafruit plastic and brass meters
-    ------> http://www.adafruit.com/products/828
-    ------> http://www.adafruit.com/products/833
+  Designed specifically to work with the Adafruit WiFi products:
+  ----> https://www.adafruit.com/products/1469
 
-Connect the red wire to +5V, 
-the black wire to common ground 
-and the yellow sensor wire to pin #2
+  Adafruit invests time and resources providing this open source code, 
+  please support Adafruit and open-source hardware by purchasing 
+  products from Adafruit!
 
-Adafruit invests time and resources providing this open source code, 
-please support Adafruit and open-source hardware by purchasing 
-products from Adafruit!
-
-Written by Limor Fried/Ladyada  for Adafruit Industries.  
-BSD license, check license.txt for more information
-All text above must be included in any redistribution
-**********************************************************/
-//#include "LiquidCrystal.h"
-//LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
-
-// which pin to use for reading the sensor? can use any pin!
-#define FLOWSENSORPIN 2
-
-// count how many pulses!
-volatile uint16_t pulses = 0;
-// track the state of the pulse pin
-volatile uint8_t lastflowpinstate;
-// you can try to keep time of how long it is between pulses
-volatile uint32_t lastflowratetimer = 0;
-// and use that to calculate a flow rate
-volatile float flowrate;
-// Interrupt is called once a millisecond, looks for any pulses from the sensor!
-SIGNAL(TIMER0_COMPA_vect) {
-  uint8_t x = digitalRead(FLOWSENSORPIN);
-  
-  if (x == lastflowpinstate) {
-    lastflowratetimer++;
-    return; // nothing changed!
-  }
-  
-  if (x == HIGH) {
-    //low to high transition!
-    pulses++;
-  }
-  lastflowpinstate = x;
-  flowrate = 1000.0;
-  flowrate /= lastflowratetimer;  // in hertz
-  lastflowratetimer = 0;
-}
-
-void useInterrupt(boolean v) {
-  if (v) {
-    // Timer0 is already used for millis() - we'll just interrupt somewhere
-    // in the middle and call the "Compare A" function above
-    OCR0A = 0xAF;
-    TIMSK0 |= _BV(OCIE0A);
-  } else {
-    // do not call the interrupt function COMPA anymore
-    TIMSK0 &= ~_BV(OCIE0A);
-  }
-}
-
-void setup() {
-   Serial.begin(9600);
-   Serial.print("Flow sensor test!");
-   //lcd.begin(16, 2);
-   
-   pinMode(FLOWSENSORPIN, INPUT);
-   digitalWrite(FLOWSENSORPIN, HIGH);
-   lastflowpinstate = digitalRead(FLOWSENSORPIN);
-   useInterrupt(true);
-}
-
-void loop()                     // run over and over again
-{ 
-  //lcd.setCursor(0, 0);
-  //lcd.print("Pulses:"); 
-  //lcd.print(pulses, DEC);
-  //lcd.print(" Hz:");
-  //lcd.print(flowrate);
-  //lcd.print(flowrate);
-  //Serial.print("Freq: "); 
-  //Serial.println(flowrate);
-  //Serial.print("Pulses: "); 
-  //Serial.println(pulses, DEC);
-  
-  // if a plastic sensor use the following calculation
-  // Sensor Frequency (Hz) = 7.5 * Q (Liters/min)
-  // Liters = Q * time elapsed (seconds) / 60 (seconds/minute)
-  // Liters = (Frequency (Pulses/second) / 7.5) * time elapsed (seconds) / 60
-  // Liters = Pulses / (7.5 * 60)
-  float liters = pulses;
-  liters /= 7.5;
-  liters /= 60.0;
-
-/*
-  // if a brass sensor use the following calculation
-  float liters = pulses;
-  liters /= 8.1;
-  liters -= 6;
-  liters /= 60.0;
-*/
-  Serial.print(liters); 
-  Serial.println(" Liters");
-  //lcd.setCursor(0, 1);
-  //lcd.print(liters); lcd.print(" Liters        ");
+  Written by Tony DiCola, based on code example code written by
+  Limor Fried & Kevin Townsend for Adafruit Industries.  
+  BSD license, all text above must be included in any redistribution
+ ****************************************************/
  
-  delay(100);
+#include "Adafruit_CC3000.h"
+#include "ccspi.h"
+#include <string.h>
+#include "debug.h"
+
+// These are the interrupt and control pins
+#define ADAFRUIT_CC3000_IRQ   3  // MUST be an interrupt pin!
+// These can be any two pins
+#define ADAFRUIT_CC3000_VBAT  5
+#define ADAFRUIT_CC3000_CS    10
+// Use hardware SPI for the remaining pins
+// On an UNO, SCK = 13, MISO = 12, and MOSI = 11
+Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
+                                         SPI_CLOCK_DIV2); // you can change this clock speed
+
+#define WLAN_SSID       "Pad"           // cannot be longer than 32 characters!
+#define WLAN_PASS       "star*buck$"
+
+// Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
+#define WLAN_SECURITY   WLAN_SEC_WPA2
+
+// Test server configuration
+const uint8_t   SERVER_IP[4]   = { 172, 16, 25, 68 };
+const uint16_t  SERVER_PORT    = 9000;
+
+#define ASSERT_EQ(expected, actual) { \
+  if (actual != expected) { \
+    Serial.print("FAILURE: Expected "); \
+    Serial.print(actual); \
+    Serial.print(" to equal "); \
+    Serial.println(expected); \
+  } \
+}
+
+#define TEST_FASTRPRINT(message, client) { \
+  Serial.print("Testing fastrprint with char* input: "); \
+  Serial.println(message); \
+  unsigned long n = client.fastrprint(message); \
+  ASSERT_EQ(sizeof(message)-1, n); \
+}
+
+#define TEST_FASTRPRINTLN(message, client) { \
+  Serial.print("Testing fastrprintln with char* input: "); \
+  Serial.println(message); \
+  unsigned long n = client.fastrprintln(message); \
+  ASSERT_EQ(sizeof(message)+1, n); \
+}
+
+#define TEST_FASTRPRINT_F(message, client) { \
+  Serial.print("Testing fastrprint with flash string input: "); \
+  Serial.println(F(message)); \
+  unsigned long n = client.fastrprint(F(message)); \
+  ASSERT_EQ(sizeof(message)-1, n); \
+}
+
+#define TEST_FASTRPRINTLN_F(message, client) { \
+  Serial.print("Testing fastrprintln with flash string input: "); \
+  Serial.println(F(message)); \
+  unsigned long n = client.fastrprintln(F(message)); \
+  ASSERT_EQ(sizeof(message)+1, n); \
+}
+
+// Run the test
+void runTest(void) {
+  // Make two connections to the server running listener.py
+  Serial.println(F("Connecting to server..."));
+  Adafruit_CC3000_Client client1 = cc3000.connectTCP(cc3000.IP2U32(SERVER_IP[0], SERVER_IP[1], SERVER_IP[2], SERVER_IP[3]), 
+                                                     SERVER_PORT);
+  if (!client1.connected()) {
+    Serial.println(F("Couldn't connect to server! Make sure listener.py is running on the server."));
+    while(1);
+  }
+  Adafruit_CC3000_Client client2 = cc3000.connectTCP(cc3000.IP2U32(SERVER_IP[0], SERVER_IP[1], SERVER_IP[2], SERVER_IP[3]), 
+                                                     SERVER_PORT);
+  if (!client2.connected()) {
+    Serial.println(F("Couldn't connect to server! Make sure listener.py is running on the server."));
+    while(1);
+  }
+  Serial.println(F("Connected!"));
+  
+  // Start the test
+  Serial.println(F("Starting tests..."));
+  unsigned long start = millis();
+  
+  // Test character strings
+  TEST_FASTRPRINT("Fastrprint string.", client1);
+  TEST_FASTRPRINT("Fastrprint with a large (>32 character) character string!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", client1);
+  TEST_FASTRPRINTLN("Fastrprintln string.", client1);
+  TEST_FASTRPRINTLN("Fastrprintln with a large (>32 character) character string!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", client1);
+  
+  // Test flash strings
+  TEST_FASTRPRINT_F("Fastrprint flash.", client1);
+  TEST_FASTRPRINT_F("Fastrprint with a large (>32 character) flash string!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", client1);
+  TEST_FASTRPRINTLN_F("Fastrprintln flash.", client1);
+  TEST_FASTRPRINTLN_F("Fastrprintln with a large (>32 character) flash string!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", client1);
+  
+  // Test empty strings
+  TEST_FASTRPRINT("", client1);
+  TEST_FASTRPRINTLN("", client1);
+  TEST_FASTRPRINT_F("", client1);
+  TEST_FASTRPRINTLN_F("", client1);
+  
+  // Test printing to multiple clients
+  TEST_FASTRPRINT("This is a message for client 1.", client1);
+  TEST_FASTRPRINT("This is a message for client 2.", client2);
+  TEST_FASTRPRINT_F("This is a flash message for client 1.", client1);
+  TEST_FASTRPRINT_F("This is a flash message for client 2.", client2);
+  TEST_FASTRPRINT_F("This is a large (>32 character) flash message for client 1!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", client1);
+  TEST_FASTRPRINT_F("This is a large (>32 character) flash message for client 2!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!", client2);
+
+  unsigned long finish = millis();
+  Serial.println(F("Tests finished!"));
+  Serial.print(F("Free RAM: ")); Serial.println(getFreeRam(), DEC);
+  Serial.print(F("Time taken to run test (MS): ")); Serial.println(finish - start, DEC);
+  
+  client1.close();
+  client2.close();
+  cc3000.disconnect();
+}
+
+// Set up the HW and the CC3000 module (called automatically on startup)
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println(F("Hello, CC3000!\n")); 
+  
+  /* Initialise the module */
+  Serial.println(F("\nInitializing..."));
+  if (!cc3000.begin())
+  {
+    Serial.println(F("Couldn't begin()! Check your wiring?"));
+    while(1);
+  }
+  
+  if (!cc3000.connectToAP(WLAN_SSID, WLAN_PASS, WLAN_SECURITY)) {
+    Serial.println(F("Failed!"));
+    while(1);
+  }
+   
+  Serial.println(F("Connected!"));
+  
+  /* Wait for DHCP to complete */
+  Serial.println(F("Request DHCP"));
+  while (!cc3000.checkDHCP())
+  {
+    delay(100); // ToDo: Insert a DHCP timeout!
+  }  
+
+  /* Display the IP address DNS, Gateway, etc. */  
+  while (! displayConnectionDetails()) {
+    delay(1000);
+  }
+  
+  runTest();
+}
+
+void loop(void)
+{
+ delay(1000);
+}
+
+// Tries to read the IP address and other connection details
+bool displayConnectionDetails(void)
+{
+  uint32_t ipAddress, netmask, gateway, dhcpserv, dnsserv;
+  
+  if(!cc3000.getIPAddress(&ipAddress, &netmask, &gateway, &dhcpserv, &dnsserv))
+  {
+    Serial.println(F("Unable to retrieve the IP Address!\r\n"));
+    return false;
+  }
+  else
+  {
+    Serial.print(F("\nIP Addr: ")); cc3000.printIPdotsRev(ipAddress);
+    Serial.print(F("\nNetmask: ")); cc3000.printIPdotsRev(netmask);
+    Serial.print(F("\nGateway: ")); cc3000.printIPdotsRev(gateway);
+    Serial.print(F("\nDHCPsrv: ")); cc3000.printIPdotsRev(dhcpserv);
+    Serial.print(F("\nDNSserv: ")); cc3000.printIPdotsRev(dnsserv);
+    Serial.println();
+    return true;
+  }
 }
