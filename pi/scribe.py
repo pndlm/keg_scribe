75852a8f15273ae996db7dbf3450b292da3a7de3
Allen Peloquin <acpeloquin@gmail.com>
2015-02-03 14:16:49 -0800

    Commit Raspberry Pi Scripts

---
+++ pi/scribe.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python
+#
+# The Keg Scribe - Translating Beer into Data
+#
+# Dependencies:
+#	requests (sudo pip install requests)
+#
+import RPi.GPIO as GPIO
+import httplib
+import threading
+import csv
+import requests
+import time
+from datetime import datetime
+from flowmeter import *
+
+# constants
+MS_IN_A_SECOND = 1000
+MIN_POUR = 0.01
+
+testUri = "http://demo.pndlm.com/"
+uploadUri = "http://hakase.pndlm.com/import/simpleCSV"
+
+fileWriteRate = 10 # append to CSV this often (s)
+fileUploadRate = 10 # upload CSV this often (s)
+
+csvFilename = "pour_values.csv"
+csvColumns = ["ImportCode", "UtcTimestamp", "Value"]
+
+importTapOneCode = "KegScribeTap1"
+importTapTwoCode = "KegScribeTap2"
+importTempOfficeCode = "KegScribeAmbientTemperature"
+importTempKegCode = "KegScribeFridgeTemperature"
+
+Tap1Pin = 17
+
+lastPinState = False
+lastPinChange = 0
+lastPour = 0
+lastUpload = 0
+
+# application state
+tap1 = FlowMeter("metric", "beer")
+
+def log(str):
+	print "[{}] - {}".format(datetime.now().time(), str)
+
+def testConnection():
+	try:
+		res = requests.get(testUri)
+		return True
+	except Exception, ex:
+		log("Error: {}".format(ex))
+	return False	
+
+def putCsvFile():
+	try:
+		uploadFile = {"file": ("ksup.csv", open(csvFilename, "r"), "test/csv")}
+		res = requests.post(uploadUri, auth=requests.auth.HTTPBasicAuth("ASDF", "BATSHIT"), files=uploadFile)
+		log(res.text)
+		if res.status_code == requests.codes.ok:
+			lastUpload = int(time.time())
+			return True
+		else:
+			log("Upload failed with response: {}".format(res.text))
+			return False
+	except Exception, ex:
+		log("Upload failed with exception: {}".format(ex))
+	return False
+
+def getSample(code, pourLiters):
+	return {"ImportCode": code, "UtcTimestamp": int(time.time()), "Value": pourLiters}
+
+def resetFile():
+	# nuke and reset the file in place
+	with open(csvFilename, "w") as csvfile:
+		writer = csv.DictWriter(csvfile, csvColumns)
+		writer.writeheader()
+
+def setupPins():
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(Tap1Pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def writePour(tapNumber, value):
+	importCode = " "
+	if tapNumber == 1:
+		importCode = importTapOneCode
+	elif tapNumber == 2:
+		importCode = importTapTwoCode
+	if tapNumber != 1: # TODO: Support tap2
+		log("Invalid tap number")
+		return
+	with open(csvFilename, "a") as csvfile:
+		writer = csv.DictWriter(csvfile, csvColumns) # DickWriter, huehuehue
+		writer.writerow(getSample(importCode, value))
+	if putCsvFile():
+		resetFile()
+		log("Upload successful!")
+	else:
+		log("Upload failed - retaining file")
+
+def hookIOEvents():
+	GPIO.add_event_detect(Tap1Pin, GPIO.RISING, callback=updateTap1, bouncetime=20)
+	# TODO: Tap2
+
+def updateTap1(channel):
+	currentTime = int(time.time() * MS_IN_A_SECOND)
+	if tap1.enabled == True:
+		tap1.update(currentTime)
+
+def updateTap2(channel):
+	pass # TODO
+
+def enterWorker():
+	log("Virtual alcohol moderator is serving, lol")
+	lastPour = 0
+	while True:
+		currentTime = int(time.time()*MS_IN_A_SECOND)
+		if tap1.thisPour > MIN_POUR and (currentTime - lastPour) > 5000:
+			log("Tap 1 Poured {} of beer".format(tap1.getFormattedThisPour()))
+			writePour(1, tap1.thisPour)			
+			lastPour = int(time.time() * MS_IN_A_SECOND)
+			tap1.thisPour = 0.0
+
+def main():
+	print "KegScribe booting..."
+	if not testConnection():
+		log("OFFLINE -- I require network, FEED ME SEYMOUR!")
+	else:
+		log("ONLINE -- Turning the ignition key")
+		resetFile() # establish file
+		setupPins() # hook input pins
+		hookIOEvents() # on edge events...
+		enterWorker() # block the parent thread so the process doesn't exit
+
+# entry point of script
+if __name__ == "__main__":
+	main()
